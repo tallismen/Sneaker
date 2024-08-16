5326324cae1ddb9cdcaccc4f6b5d41cc04e4cc11
Maarten Lammers <maarten.lammers@themobilecompany.com>
2024-08-16 14:06:44 +0200

    Update gradle files and versions to latest android

--- app/src/main/java/com/irozon/sneakersample/MainFragment.kt
+++ app/src/main/java/com/irozon/sneakersample/MainFragment.kt
@@ -6,13 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.irozon.sneaker.Sneaker
-import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
@@ -20,12 +19,12 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btShowWarning.setOnClickListener {
+        this.view?.findViewById<View>(R.id.btShowWarning)?.setOnClickListener {
             Sneaker.with(this)
-                    .setTitle("Warning!!")
-                    .setCornerRadius(5, 5)
-                    .setMessage("This is the warning message")
-                    .sneakWarning()
+                .setTitle("Warning!!")
+                .setCornerRadius(5, 5)
+                .setMessage("This is the warning message")
+                .sneakWarning()
         }
     }
 }
--- sneaker/src/main/java/com/irozon/sneaker/SneakerView.kt
+++ sneaker/src/main/java/com/irozon/sneaker/SneakerView.kt
@@ -11,6 +11,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import com.irozon.sneaker.Utils.customView
 import com.irozon.sneaker.widget.RoundedImageView
+import com.irozon.sneakersample.R
 
 internal class SneakerView(context: Context?) : LinearLayout(context) {
     init {
